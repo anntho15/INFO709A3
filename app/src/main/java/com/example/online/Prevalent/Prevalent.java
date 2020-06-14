d19ff1ba9b4452266ee9174067e81bcd26aecfb8
ann.thomas.brelext <ann.thomas.brelext@gmail.com>
2020-06-14 21:21:28 +1200

    Initial commit

---
+++ app/src/main/java/com/example/online/Prevalent/Prevalent.java
@@ -0,0 +1,10 @@
+package com.example.online.Prevalent;
+
+import com.example.online.Model.Users;
+
+public class Prevalent {
+    public static Users currentOnlineUser = new Users("a","2","3","4","5");
+
+    public static final String UserPhoneKey = "UserPhone";
+    public static final String UserPasswordKey = "UserPassword";
+}
